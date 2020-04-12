a6f8a5f2b610a4e716bc1d93733f61057f358a58
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-12 18:13:19 +0530

    distance formula added

--- point.c
+++ point.c
@@ -1,13 +1,19 @@
+#include<math.h>
 #include "point.h"
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location) {
+  float distance = 0, value = 0;
+  int id;
   for(int i = 0; i < points_length; i++) {
-    if(current_location.x == food_points[i].x) {
-    closest_food_location->x = food_points[i].x;
-    closest_food_location->y = food_points[i].y;
+    distance = sqrt(pow((food_points[i].x - current_location.x),2) + pow((food_points[i].y - current_location.y),2));
+    if(value < distance) {
+      id = i;
+      value = distance;
+    }
   }
-  }
+  closest_food_location->x = food_points[id].x;
+  closest_food_location->y = food_points[id].y;
 }
 
 // {{18,76},{19,66},{89,57},{9,71},{55,38}};
-// 18 86
+// 69 7 =>  [55 38]
