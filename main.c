2a4c5596e8e9c5fc9a8b23ad0df76e5f1f1af2bd
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-12 18:53:43 +0530

    added food_points[0]

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(void)
 {
   Point food_points[] = {{18,76},{19,66},{89,57},{9,71},{55,38}};
-  int points_length = sizeof(food_points) / sizeof(Point);
+  int points_length = sizeof(food_points) / sizeof(food_points[0]);
   Point current_location = {36,27}; 
   Point closest_food_location;
   get_closest_food(food_points, points_length, current_location, &closest_food_location);
